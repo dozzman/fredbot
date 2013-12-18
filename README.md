31c214b26f957aefc00d2def2c97307a7931f8ce
Dorian Peake <dorian@vereia.com>
2013-12-18 15:23:03 +0000

    adjusted readme and gitignore files, removed smack (link in readme)

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Just a really simple, XMPP chatbot which connects to our HipChat server.
 Doesn't do much at the moment, but theres room for expansion!
+
+## Requirements
+
+This project uses smack.jar and smackx.jar from the Smack XMPP client library provided by [Ignite Realtime][http://www.igniterealtime.org/downloads/index.jsp]. The jar files will need to be included in the classpath when compiling the software.
+
+Apart from that, everything should be good to go!
--- src/fredbot/HelpChatEvent.java
+++ src/fredbot/HelpChatEvent.java
@@ -8,7 +8,11 @@ public class HelpChatEvent extends ChatEvent {
 	public void process(ChatEventCallback callback) {
 		// TODO Auto-generated method stub
 		Message msg = new Message();
-		callback.chatEventCallback("No help available at the moment!");
+		callback.chatEventCallback("Available commands (preceded by '@fred'):\n"
+				+ "hello - Say hi to me :)\n"
+				+ "paper YYYYpPPqQQ - get a link for a specific paper\n"
+				+ "help - display this message"
+				);
 	}
 
 }
